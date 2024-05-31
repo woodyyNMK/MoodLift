1fc08296417a1d77246009804fea984487f48ad6
WooDyy <woodyy.nmk@gmail.com>
2024-05-31 11:10:52 +0700

    [ADD] Initial Firebase Setup
    - add .env
    - integrated with Firebase
    - tested with sample input
    - still need to integrate and test after UI.dart is done.

--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,37 @@ from flask import Flask
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
+import pyrebase
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
 client = MongoClient(app.config["MONGO_URI"], server_api=ServerApi('1'))
 db = client["sample_mflix"]
+
+firebaseconfig = {
+    "apiKey": app.config["APIKEY"],
+    "authDomain": app.config["AUTHDOMAIN"],
+    "databaseURL": "",
+    "storageBucket": app.config["STORAGEBUCKET"],
+    "projectId": app.config["PROJECTID"],
+    "messagingSenderId": app.config["MESSAGINGSENDERID"],
+    "appId": app.config["APPID"],    
+}
+# Initialize Firebase
+firebase = pyrebase.initialize_app(firebaseconfig)
+
+# Get reference to the auth service and database service
+auth = firebase.auth()
+
+
+email = input("Enter email: ")
+password = input("Enter password: ")
+# user = auth.create_user_with_email_and_password(email, password)
+user = auth.sign_in_with_email_and_password(email, password)
+# print(user)
+print(user['idToken'])
+info = auth.get_account_info(user['idToken'])
+print(info)
 
 @app.route("/")
 def home_page():
--- backend/settings.py
+++ backend/settings.py
@@ -1,2 +1,8 @@
 from os import environ
 MONGO_URI=environ.get("MONGO_URI")
+APIKEY=environ.get("apiKey")
+AUTHDOMAIN=environ.get("authDomain")
+STORAGEBUCKET=environ.get("storageBucket")
+PROJECTID=environ.get("projectId")
+APPID=environ.get("appId")
+MESSAGINGSENDERID=environ.get("messagingSenderId")
