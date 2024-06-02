66df1c824f04e79eb9e8d30257d584934c1d40d4
woodyy <woodyy.nmk@gmail.com>
2024-06-02 14:30:11 +0700

    [ADD] firebase AUTH functions
    -login
    -logout
    -register

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,11 @@
-from flask import Flask
-from flask_pymongo import PyMongo
+from flask import Flask, request, session
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 import pyrebase
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
-client = MongoClient(app.config["MONGO_URI"], server_api=ServerApi('1'))
+client = MongoClient(app.config["MONGO_URI"])
 db = client["sample_mflix"]
 
 firebaseconfig = {
@@ -24,7 +23,7 @@ firebase = pyrebase.initialize_app(firebaseconfig)
 # Get reference to the auth service and database service
 auth = firebase.auth()
 
-
+'''
 email = input("Enter email: ")
 password = input("Enter password: ")
 # user = auth.create_user_with_email_and_password(email, password)
@@ -33,8 +32,38 @@ user = auth.sign_in_with_email_and_password(email, password)
 print(user['idToken'])
 info = auth.get_account_info(user['idToken'])
 print(info)
+'''
 
-@app.route("/")
+@app.route("/test")
 def home_page():
     movie = db.movies.find_one()
     return movie["title"]
+
+@app.route("/", methods=['POST','GET' ])
+def login():
+    if 'user' in session:
+        return "You are logged in as " + session['user']
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            user = auth.sign_in_with_email_and_password(email, password)
+            session['user']=user['email']
+            return user['idToken'], 200
+        except:
+            return "Invalid credentials", 401
+        
+@app.route("/logout")
+def logout():
+    session.pop('user', None)
+    return "Logged Out", 200
+
+@app.route("/register", methods=['POST'])
+def register():
+    email = request.form['email']
+    password = request.form['password']
+    try:
+        user = auth.create_user_with_email_and_password(email, password)
+        return user['idToken'], 200
+    except:
+        return "Email already exists", 401
