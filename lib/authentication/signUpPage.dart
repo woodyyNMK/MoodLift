34cc6a0b0601566638be414733fa2e1a0a21f99b
Vincent-NHtun <naylinhtunclifton1466@gmail.com>
2024-06-03 15:30:41 +0700

    [ADD] DiaryPage and Detail Page

--- lib/authentication/signUpPage.dart
+++ lib/authentication/signUpPage.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
